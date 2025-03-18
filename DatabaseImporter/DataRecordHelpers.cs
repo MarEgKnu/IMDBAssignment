13feaa2d3e57b6798261febf69b6ad15370e7ddf
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-03-18 17:44:34 +0100

    wip changing peopleInserter impl

---
+++ DatabaseImporter/DataRecordHelpers.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseImporter
+{
+    public static class DataRecordHelpers
+    {
+        const int INITIAL_CAPACITY = 14211906; //How much list capacity to allocate to start with
+        const int NCONST_MAX_SIZE = 12;
+        const int PRIMARY_NAME_MAX_SIZE = 70;
+        const int YEAR_MAX_SIZE = 4;
+        const int PRIMARY_PROF_MAX_SIZE = 75;
+        const int KNOWN_FOR_TITLES_MAX_SIZE = 48;
+        private static SqlMetaData[] _peopleMetaData = new SqlMetaData[]
+        {
+            new SqlMetaData("nconst", System.Data.SqlDbType.VarChar, NCONST_MAX_SIZE),
+            new SqlMetaData("primaryName", System.Data.SqlDbType.VarChar, PRIMARY_NAME_MAX_SIZE),
+            new SqlMetaData("birthYear", System.Data.SqlDbType.VarChar, YEAR_MAX_SIZE),
+            new SqlMetaData("deathYear", System.Data.SqlDbType.VarChar, YEAR_MAX_SIZE),
+            new SqlMetaData("primaryProfession", System.Data.SqlDbType.VarChar, PRIMARY_PROF_MAX_SIZE),
+            new SqlMetaData("knownForTitles", System.Data.SqlDbType.VarChar, KNOWN_FOR_TITLES_MAX_SIZE),
+
+        };
+        public static IEnumerable<SqlDataRecord> CreateDataRecordsPeople(StreamReader sr)
+        {
+            sr.ReadLine(); // read the first line as it is useless
+            List<SqlDataRecord> records = new List<SqlDataRecord>(INITIAL_CAPACITY);
+            while (!sr.EndOfStream)
+            {
+                string[] fields = sr.ReadLine().Split('\t');
+                ValidatePeopleFields(fields);
+                SqlDataRecord record = new SqlDataRecord(_peopleMetaData);
+                record.SetString(0, fields[0]);
+                record.SetString(1, fields[1]);
+                record.SetString(2, fields[2]);
+                record.SetString(3, fields[3]);
+                record.SetString(4, fields[4]);
+                record.SetString(5, fields[5]);
+                records.Add(record);
+            }
+            return records;
+
+        }
+
+
+
+
+        private static void ValidatePeopleFields(string[] fields)
+        {
+            if (fields.Length != 6)
+            {
+                throw new InvalidDataException($"{fields.Length} fields were detected, not 6");
+            }
+            else if (fields[0].Length > NCONST_MAX_SIZE)
+            {
+                throw new InvalidDataException($"nconst field {fields[0]} was longer than {NCONST_MAX_SIZE} characters long");
+            }
+            else if (fields[1].Length > PRIMARY_NAME_MAX_SIZE)
+            {
+                throw new InvalidDataException($"primaryName field {fields[1]} was longer than {PRIMARY_NAME_MAX_SIZE} characters long");
+            }
+            else if (fields[2].Length > YEAR_MAX_SIZE)
+            {
+                throw new InvalidDataException($"birthYear field {fields[2]} was longer than {YEAR_MAX_SIZE} characters long");
+            }
+            else if (fields[3].Length > YEAR_MAX_SIZE)
+            {
+                throw new InvalidDataException($"deathYear field {fields[3]} was longer than {YEAR_MAX_SIZE} characters long");
+            }
+            else if (fields[4].Length > PRIMARY_PROF_MAX_SIZE)
+            {
+                throw new InvalidDataException($"primaryProfession field {fields[4]} was longer than {PRIMARY_PROF_MAX_SIZE} characters long");
+            }
+            else if (fields[5].Length > KNOWN_FOR_TITLES_MAX_SIZE)
+            {
+                throw new InvalidDataException($"knownForTitles field {fields[5]} was longer than {KNOWN_FOR_TITLES_MAX_SIZE} characters long");
+            }
+
+        }
+    }
+}
--- DatabaseImporter/Program.cs
+++ DatabaseImporter/Program.cs
@@ -6,11 +6,13 @@ using System.Net.Sockets;
 
 DatabaseInserter titlesInserter = new TitlesInserter();
 DatabaseInserter profInserter = new ProfessionInserter();
+DatabaseInserter pplInserter = new PeopleInserter();
 using(SqlConnection connection = new SqlConnection(Secret.ConnectionString))
 {
     connection.Open();
+    pplInserter.Insert("C:\\Users\\Marius\\Downloads\\name.basics.tsv\\name.basics.tsv", connection);
     //titlesInserter.Insert("C:\\Users\\Marius\\Downloads\\title.basics.tsv\\title.basics.tsv", connection);
-    profInserter.Insert("C:\\Users\\Marius\\Downloads\\name.basics.tsv\\name.basics.tsv", connection);
+    //profInserter.Insert("C:\\Users\\Marius\\Downloads\\name.basics.tsv\\name.basics.tsv", connection);
 
 }
 
