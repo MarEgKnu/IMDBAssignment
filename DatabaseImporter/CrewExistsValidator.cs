7a3095a3a424a1ded8050ae2c6229d3d90aaac9a
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-04-03 14:59:30 +0200

    changed crew validator

--- DatabaseImporter/CrewExistsValidator.cs
+++ DatabaseImporter/CrewExistsValidator.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient.Server;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,59 +14,61 @@ namespace DatabaseImporter
     {
         const int BATCHES = 1000;
         const int TCONST_MAX_SIZE = 12;
-        const int NCONST_MAX_SIZE = 12;
-        const int CATEGORY_MAX_SIZE = 20;
-        const int JOB_MAX_SIZE = 290;
-        const int CHARACTERS_MAX_SIZE = 465;
+        const int WRITERS_MAX_SIZE = 350;
+        const int DIRECTORS_MAX_SIZE = 350;
         private static SqlMetaData[] _crewMetaData = new SqlMetaData[]
         {
             new SqlMetaData("tconst", System.Data.SqlDbType.VarChar, TCONST_MAX_SIZE),
-            new SqlMetaData("directors", System.Data.SqlDbType.VarChar, NCONST_MAX_SIZE),
-            new SqlMetaData("writers", System.Data.SqlDbType.VarChar, NCONST_MAX_SIZE),
+            new SqlMetaData("directors", System.Data.SqlDbType.VarChar, DIRECTORS_MAX_SIZE),
+            new SqlMetaData("writers", System.Data.SqlDbType.VarChar, WRITERS_MAX_SIZE),
 
 
         };
         public override void Validate(string filePath, SqlConnection connection = null)
         {
+            int index = -1;
             IEnumerable<string> lines = File.ReadLines(filePath).Skip(1);
-            List<SqlDataRecord> paramBuffer = new List<SqlDataRecord>(lines.Count() * 3);
+            List<SqlDataRecord> paramBuffer = new List<SqlDataRecord>(lines.Count() / BATCHES);
             //string[] lines = File.ReadAllLines(filePath).Skip(1).ToArray();
             foreach (string line in lines)
             {
                 string[] fields = line.Split('\t');
+                SqlDataRecord record = new SqlDataRecord(_crewMetaData);
+                index++;
+
                 if (fields[1].IsNullString() && fields[2].IsNullString())
                 {
                     continue;
                 }
-                string[] directors = fields[1].Split(",");
-                string[] writers = fields[2].Split(",");
+
+                record.SetString(0, fields[0]);
                 if (fields[1].IsNullString())
                 {
-                    for(int i = 0; i < writers.Length; i++)
-                    {
-                        paramBuffer.Add(CreateCrewRecord(fields[0], null, writers[i]));
-                    }
-                }
-                else if (fields[2].IsNullString())
-                {
-                    for (int i = 0; i < directors.Length; i++)
-                    {
-                        paramBuffer.Add(CreateCrewRecord(fields[0], directors[i], null));
-                    }
+                    record.SetDBNull(1);
                 }
                 else
                 {
-                    for (int i = 0; i < writers.Length; i++)
-                    {
-                        for (int j = 0; j < directors.Length; j++)
-                        {
-                            paramBuffer.Add(CreateCrewRecord(fields[0], directors[j], writers[i]));
-                        }
-                    }
+                    record.SetString(1, fields[1]);
                 }
-                
-            }
-            int x = 5;
+                if (fields[2].IsNullString())
+                {
+                    record.SetDBNull(2);
+                }
+                else 
+                {
+                    record.SetString(2, fields[2]);
+                }
+
+                paramBuffer.Add(record);
+
+                if ((index + 1) % paramBuffer.Capacity == 0)
+                {
+                    SqlCommand cmd = new SqlCommand("InsertPrincipalsLightBulk", connection) { CommandType = CommandType.StoredProcedure, CommandTimeout = 60 };
+                    SqlParameter param = new SqlParameter("@InData", SqlDbType.Structured) { TypeName = "dbo.RawCrewData", Value = paramBuffer };
+                    cmd.Parameters.Add(param);
+                    cmd.ExecuteNonQuery();
+                    paramBuffer.Clear();
+                }
         }
         private SqlDataRecord CreateCrewRecord(string tconst, string? director, string? writer)
         {
